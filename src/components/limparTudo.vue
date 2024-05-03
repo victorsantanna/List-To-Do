5fface3e3dcde925801ae13d4dfead3ca6346271
victorsantanna <victor.esef@hotmail.com>
2024-05-02 22:29:55 -0300

    feat: adiçao do button Limpar tudo, adição do script  e style do componente LimparTudo.

---
+++ src/components/limparTudo.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="clear" @click="clearTodos" >Limpar tudo </button>
+</template>
+
+<script>
+export default {
+    props:{
+        todos: Array,
+    },
+    methods:{
+        clearTodos(){
+            this.$emit('clear');
+        }
+    }
+    
+};
+</script>
+
+<style scoped>
+button.clear {
+    background-color: #35d1dc;
+    border: 1px solid #35d1dc;
+    display: block;
+    margin: auto;
+    margin-top: 20px;
+    width: 20%;
+    font-size: 19px;
+}
+
+</style>
