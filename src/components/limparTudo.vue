757e81eb75542957cb2ff580ef3e8f8c9a1446ee
victorsantanna <victor.esef@hotmail.com>
2024-05-07 09:57:35 -0300

    style: adição do :hover no button.clear e  adição do Media Queries.

--- src/components/limparTudo.vue
+++ src/components/limparTudo.vue
@@ -17,14 +17,32 @@ export default {
 </script>
 
 <style scoped>
-button.clear {
-    background-color: #dc3545;
-    border: 1px solid #dc3545;
-    display: block;
-    margin: auto;
-    margin-top: 20px;
-    width: 20%;
-    font-size: 19px;
+    button.clear {
+        background-color: #dc3545;
+        border: 1px solid #dc3545;
+        display: block;
+        margin: auto;
+        margin-top: 20px;
+        width: 300px;
+        font-size: 19px;
+    }
+
+    button.clear:hover{
+        background-color: #f00018;
+        border: 1px solid #f00018;
+    }
+
+@media (max-width: 661px){
+    
+    button.clear {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin-top: auto; 
+        margin-bottom: auto; 
+        width: 300px;
+    }
 }
 
 </style>
